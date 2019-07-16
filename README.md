48b8def9aa999022569a54c70c75714dc418e4ef
Gert Drapers <gert.drapers@live.com>
2019-07-16 16:10:06 -0700

    1.0.0-beta.0

---
+++ README.md
@@ -0,0 +1,50 @@
+# homebrew-tap
+
+Splunk Homebrew Tap
+
+This is the repository for Splunk Homebrew packages. Homebrew is "the missing package manager for OS X" (https://brew.sh/).
+
+
+## Installation
+
+To register the Splunk tap
+
+```
+brew tap splunk/tap
+```
+
+To list registered taps:
+
+```
+brew tap 
+```
+
+To install a package, like scloud:
+
+```
+brew install scloud
+```
+or
+```
+brew install splunk/tap/scloud
+```
+
+To uninstall a package, like scloud:
+
+```
+brew uninstall scloud
+```
+
+To update to the latest version:
+```
+brew upgrade scloud
+```
+
+
+## Available packages
+
+| Package | Description                                     |
+|:------- |:----------------------------------------------- |
+| scloud  | The official Splunk Cloud Platform CLI (scloud) |
+
+
---
+++ scloud.rb
@@ -0,0 +1,24 @@
+class Scloud < Formula
+  desc "Splunk Cloud Platform CLI"
+  homepage "https://github.com/splunk/scloud"
+  version "1.0.0-beta.0"
+
+  if OS.mac?
+    head "https://github.com/splunk/scloud.git"
+    url "https://github.com/splunk/scloud/releases/download/1.0.0-beta.0/scloud_1.0.0-beta.0_darwin_amd64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "2ed5345f863ee01df3796b8fc693a89b2fc2c7e240b6b574fbc7176605693ab4"
+  elsif OS.linux?
+    head "https://github.com/splunk/scloud.git"
+    url "https://github.com/splunk/scloud/releases/download/1.0.0-beta.0/scloud_1.0.0-beta.0_linux_amd64.tar.gz", :using => CustomGitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "258f5dd0be0e8948ba2e0505c39eaed1145fcbb30f5a23e12a7f9542d2a008a3"
+  end
+
+  def install
+    bin.install "scloud"
+  end
+
+  # Homebrew requires tests.
+  test do
+    system "#{bin}/scloud version"
+  end
+end

a3c93f15cf17a3a37c744a52a7c0dc7f76ac41ee
Chris Pride <cpride@splunk.com>
2020-03-28 07:45:18 -0700

    Update the version of Scloud

--- scloud.rb
+++ scloud.rb
@@ -2,16 +2,16 @@ class Scloud < Formula
 
   desc "Splunk Cloud Platform CLI"
   homepage "https://github.com/splunk/scloud"
-  version "1.3.0"
+  version "2.0.0"
 
   if OS.mac?
     head "https://github.com/splunk/scloud.git"
-    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.3.0/scloud_v1.3.0_darwin_amd64.tar.gz"
-    sha256 "e132c71003fd3598098c6195270eb9cd6750eae76a62a83f25fdd6f9c0ae72fb"
+    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.4.0/scloud_v2.0.0_darwin_amd64.tar.gz"
+    sha256 "166d12e86fada73702c438193eca3bbe99c73e83fbf278b19192742e6c0c8c5d"
   elsif OS.linux?
     head "https://github.com/splunk/scloud.git"
-    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.3.0/scloud_v1.3.0_linux_amd64.tar.gz"
-    sha256 "79239d379ebd3b0b6a8024aa87b46e69cb426891b028bbfed0c16a7266efcf79"
+    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.4.0/scloud_v2.0.0_linux_amd64.tar.gz"
+    sha256 "d8b99fbbcd8d7b5b19e7a711c93db2a451969cdc8e018c6da3efa5e392d98bf4"
   end
 
   def install

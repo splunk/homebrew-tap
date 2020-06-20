97b3c346ff9cb46df104e60718a4e46729687fdb
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-20 14:02:12 -0700

    Brew formula update for qbec version v0.12.1

--- qbec.rb
+++ qbec.rb
@@ -2,16 +2,16 @@
 class Qbec < Formula
   desc "Qbec (pronounced like the Canadian province) is a CLI tool that allows you to create Kubernetes objects on multiple Kubernetes clusters or namespaces configured correctly for the target environment in question."
   homepage "https://qbec.io/"
-  version "0.12.0"
+  version "0.12.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/splunk/qbec/releases/download/v0.12.0/qbec-darwin-amd64.tar.gz"
-    sha256 "6664f12cf1d8df601b053e4dbceafec443b9f5f444f49f3d7c7975c0dc8dd6c1"
+    url "https://github.com/splunk/qbec/releases/download/v0.12.1/qbec-darwin-amd64.tar.gz"
+    sha256 "b8325acfd137df0eaea870a83af54538d2c4fad33e0018cecd40a12f77dc16e2"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/splunk/qbec/releases/download/v0.12.0/qbec-linux-amd64.tar.gz"
-      sha256 "42ca4b9d3e732dfd28e3860d0cc9f1a92cd802fc4ee296516d271bafce9b3c7d"
+      url "https://github.com/splunk/qbec/releases/download/v0.12.1/qbec-linux-amd64.tar.gz"
+      sha256 "6fbec2c33ff15c603585a22c100aa839461680cf775a1f90336078bf79c6a42f"
     end
   end
 

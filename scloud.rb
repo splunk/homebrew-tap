659a63a7aa70171bf82aae92774aeac25f0234c2
srv-dev-platform <srv-dev-platform@splunk.com>
2021-10-13 18:59:59 +0000

    update scloud.rb for new release

--- scloud.rb
+++ scloud.rb
@@ -2,16 +2,16 @@ class Scloud < Formula
 
   desc "Splunk Cloud Platform CLI"
   homepage "https://github.com/splunk/scloud"
-  version "8.0.0-beta.5"
+  version "8.0.0-beta.6"
 
   if OS.mac?
     head "https://github.com/splunk/scloud.git"
-    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.12.0-beta.5/scloud_v8.0.0-beta.5_darwin_amd64.tar.gz"
-    sha256 "84f9ce049db17329e457417950101496f4affda1acc7dfd99228edb97deebc01"
+    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.12.0-beta.6/scloud_v8.0.0-beta.6_darwin_amd64.tar.gz"
+    sha256 "42385e4f7a2f0b0c537d7d6152605e6ec3ea8de4a3d35a4947b279626884ad80"
   elsif OS.linux?
     head "https://github.com/splunk/scloud.git"
-    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.12.0-beta.5/scloud_v8.0.0-beta.5_linux_amd64.tar.gz"
-    sha256 "6dcc8e40f442789a2ee329fabb5c9cdad873f9c896fe5604b7dce80e9c738e05"
+    url "https://github.com/splunk/splunk-cloud-sdk-go/releases/download/v1.12.0-beta.6/scloud_v8.0.0-beta.6_linux_amd64.tar.gz"
+    sha256 "36d56cbd77d1528ce4d1666cfb3eaa90bb58b2d5213ba6b12e5283d335423bcc"
   end
 
   def install

a02ac352a01be3b4c43b99a0e1156345ab5061db
rsatalkar <rsatalkar@splunk.com>
2025-10-01 21:03:28 +0000

    update acs.rb for new release

--- acs.rb
+++ acs.rb
@@ -2,14 +2,14 @@ class Acs < Formula
 
   desc "Splunk Admin Config Service CLI"
   homepage "https://github.com/splunk/acs-cli"
-  version "2.20.0"
+  version "2.20.1"
 
   if OS.mac?
-    url "https://github.com/splunk/acs-cli/releases/download/v2.20.0/acs_v2.20.0_darwin_amd64.tar.gz"
-    sha256 "741e6d94028506b95848156375cbfac3b51e146fd33150c8ad30b33b058ab50a"
+    url "https://github.com/splunk/acs-cli/releases/download/v2.20.1/acs_v2.20.1_darwin_amd64.tar.gz"
+    sha256 "76126ec2be27684d70cc988853e398ca6d203f4b33fb51e7a95a1624a49122a8"
   elsif OS.linux?
-    url "https://github.com/splunk/acs-cli/releases/download/v2.20.0/acs_v2.20.0_linux_amd64.tar.gz"
-    sha256 "8cfc896f80cf0dac975f6bbb53437b9602bd8d6c096ba50c244939af70cda00d"
+    url "https://github.com/splunk/acs-cli/releases/download/v2.20.1/acs_v2.20.1_linux_amd64.tar.gz"
+    sha256 "4cf0165fa02bc02f39551ca17d239d088aae665a300fb69dfec78b4b1ae78119"
   end
 
   def install

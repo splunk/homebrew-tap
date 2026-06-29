f14c125f1904b4b737e64ac83cf2fc17350ffb82
gthapliyal <gthapliyal@splunk.com>
2026-06-29 19:12:09 +0000

    update acs.rb for new release

--- acs.rb
+++ acs.rb
@@ -2,23 +2,23 @@ class Acs < Formula
 
   desc "Splunk Admin Config Service CLI"
   homepage "https://github.com/splunk/acs-cli"
-  version "2.23.0"
+  version "2.24.0"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/splunk/acs-cli/releases/download/v2.23.0/acs_v2.23.0_darwin_arm64.tar.gz"
-      sha256 "08a001757d4208041bd20c8756f147cf4ea6186b2ec0e5fcc0477a26fe6e1b30"
+      url "https://github.com/splunk/acs-cli/releases/download/v2.24.0/acs_v2.24.0_darwin_arm64.tar.gz"
+      sha256 "93cee86ad44df757eec41e40a78442e3deaf1502fdb3d6c39041831d26d20805"
     else
-      url "https://github.com/splunk/acs-cli/releases/download/v2.23.0/acs_v2.23.0_darwin_amd64.tar.gz"
-      sha256 "214af04cac6d24d2dd4e4864b522a8b02256885fa12cc9c4b61214549dc73306"
+      url "https://github.com/splunk/acs-cli/releases/download/v2.24.0/acs_v2.24.0_darwin_amd64.tar.gz"
+      sha256 "88ab5fe7328caf0e6207ff5cca98376e4dc831ae09449d350362c369cb734566"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/splunk/acs-cli/releases/download/v2.23.0/acs_v2.23.0_linux_arm64.tar.gz"
-      sha256 "4d9e5169a1f026418cac3a8ed1e045f7846053e1d96b55d1056d88b8256670a1"
+      url "https://github.com/splunk/acs-cli/releases/download/v2.24.0/acs_v2.24.0_linux_arm64.tar.gz"
+      sha256 "6dbddce7132303bb4454df9429726b2390e662daf6708682689dc1f36f0312b7"
     else
-      url "https://github.com/splunk/acs-cli/releases/download/v2.23.0/acs_v2.23.0_linux_amd64.tar.gz"
-      sha256 "6ccd3bb1bcdf89c0dd978354ae6e1c0bc193fc932b8c53106083713bbe2e28ee"
+      url "https://github.com/splunk/acs-cli/releases/download/v2.24.0/acs_v2.24.0_linux_amd64.tar.gz"
+      sha256 "fd9871c87e338d730d881a13498bca22ac22a102c89edf658a122a3c58f26f53"
     end
   end
 
